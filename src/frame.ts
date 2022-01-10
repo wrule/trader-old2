0abf22ec208461a24c9c1de1d90080303b680270
jimao <jimao@jimaodeMacBook-Pro.local>
2022-01-10 16:13:55 +0800

    数据帧

---
+++ src/frame.ts
@@ -0,0 +1,16 @@
+
+export
+interface IFrame {
+  time: number;
+  price: number;
+}
+
+export
+interface IFrameData {
+  time: number;
+  open: number;
+  close: number;
+  low: number;
+  high: number;
+  volume: number;
+}
