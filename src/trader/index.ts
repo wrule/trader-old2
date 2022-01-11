4348ba0e4214547ac2489e8e57015052f22d53fc
jimao <jimao@jimaodeMBP.lan>
2022-01-11 23:25:21 +0800

    交易者类完成

--- src/trader/index.ts
+++ src/trader/index.ts
@@ -1,10 +1,19 @@
-import { Bill } from "../bill";
-import { IFrame } from "../frame";
+import { Bill } from '../bill';
+import { IFrame } from '../frame';
 
+/**
+ * 交易者类
+ */
 export
 class Trader {
+  /**
+   * 构造函数
+   * @param initFunds 初始资金
+   * @param buyFee 购买费率
+   * @param sellFee 出售费率
+   */
   public constructor(
-    private initFunds = 1,
+    private initFunds = 100,
     private buyFee = 0.998,
     private sellFee = 0.998,
   ) {
@@ -13,17 +22,25 @@ class Trader {
 
   private funds = 0;
   private assets = 0;
-
   private bill = new Bill();
 
+  /**
+   * 当前资金量
+   */
   public get Funds() {
     return this.funds;
   }
 
+  /**
+   * 当前资产量
+   */
   public get Assets() {
     return this.assets;
   }
 
+  /**
+   * 交易账单
+   */
   public get Bill() {
     return this.bill;
   }
@@ -35,6 +52,10 @@ class Trader {
     return this.assets > 0;
   }
 
+  /**
+   * 购买资产
+   * @param frame 帧
+   */
   public Buy(frame: IFrame) {
     if (!this.Holding) {
       this.assets = this.funds / frame.price * this.buyFee;
@@ -43,11 +64,24 @@ class Trader {
     }
   }
 
+  /**
+   * 出售资产
+   * @param frame 帧
+   */
   public Sell(frame: IFrame) {
     if (this.Holding) {
       this.funds = this.assets * frame.price * this.sellFee;
       this.bill.RecordSell(frame, this);
       this.assets = 0;
     }
+  }
+
+  /**
+   * 重置交易者
+   */
+  public Reset() {
+    this.funds = this.initFunds;
+    this.assets = 0;
+    this.bill = new Bill();
   }
 }
