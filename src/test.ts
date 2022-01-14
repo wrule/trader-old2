8117794f5672c85ce6a5fae90465cdc68c1d79fd
jimao <jimao@jimaodeMacBook-Pro.local>
2022-01-14 17:48:12 +0800

    提交代码

--- src/test.ts
+++ src/test.ts
@@ -3,13 +3,15 @@ import { Load } from './data/coinmarketcap';
 import { Trader } from './trader';
 import { MACDFinder } from './finder/MACDFinder';
 import { MACDFinder2 } from './finder/MACDFinder2';
+import { nums } from '@wrule/nums';
+import { Cross2Line } from './strategy/Cross2Line';
 
 const trader = new Trader(100, 0.998, 0.998);
 const frames = Load(BTCData);
-
-const finder = new MACDFinder(trader, frames, 100, 50);
-const bills = finder.Find();
-bills.forEach((bill) => {
-  bill.LogSummary();
-});
-bills[0].LogAll();
+const prices = nums(frames.map((frame) => frame.price));
+const maFast = prices.MA(8);
+const maSlow = prices.MA(44);
+const strategy = new Cross2Line(trader, maFast, maSlow);
+const bill = strategy.Backtesting(frames);
+// bill.LogAll();
+console.log(bill.BillItems.map((item) => item.ProfitRate));
